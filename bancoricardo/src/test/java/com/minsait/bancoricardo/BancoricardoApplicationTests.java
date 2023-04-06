dd124bd6216ca38922554d362ba4cceaf92b3234
ric_f <ric_fisica@yahoo.com.br>
2023-04-06 17:43:29 -0300

    Upload inicial

---
+++ bancoricardo/src/test/java/com/minsait/bancoricardo/BancoricardoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.bancoricardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoricardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
