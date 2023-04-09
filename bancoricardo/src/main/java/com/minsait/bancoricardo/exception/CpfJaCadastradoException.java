8a05f403bb556ff39f2297b9924bea97da0d1923
ric_f <ric_fisica@yahoo.com.br>
2023-04-09 19:02:18 -0300

    requisicao impedida por regra de negocio > HttpStatus.EXPECTATION_FAILED

--- bancoricardo/src/main/java/com/minsait/bancoricardo/exception/CpfJaCadastradoException.java
+++ bancoricardo/src/main/java/com/minsait/bancoricardo/exception/CpfJaCadastradoException.java
@@ -3,7 +3,7 @@ package com.minsait.bancoricardo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+@ResponseStatus(HttpStatus.EXPECTATION_FAILED)
 public class CpfJaCadastradoException extends Exception {
 	private static final long serialVersionUID = 1L;
 
